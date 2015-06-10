303b056327a6f34f2aecace9e189eceee3afc85a
unknown <MM1755@CILABS-119A-07.scilsnet.rutgers.edu>
2015-06-09 20:19:21 -0400

    Complete Boid Example

---
+++ Assets/BoidSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoidSpawner : MonoBehaviour {
+
+	static public BoidSpawner  S;
+
+	public int numBoids = 100;
+	public GameObject boidPrefab;
+	public float spawnRadius = 100f;
+	public float spawnVelocity = 10f;
+	public float minVelocity = 0f;
+	public float maxVelocity = 30f;
+	public float nearDist = 30f;
+	public float collisionDist = 5f;
+	public float velcoityMatchingAmt = 0.01f;
+	public float flockCenteringAmt = 0.15f;
+	public float collisionAvoidanceAmt = -0.5f;
+	public float mouseAttractionAmt = 0.01f;
+	public float mouseAvoidanceAmt = 0.75f;
+	public float mouseAvoidanceDist = 15f;
+	public float velcoityLerpAmt = 0.25f;
+
+	public bool _________;
+
+	public Vector3 mousePos;
+	// Use this for initialization
+	void Start () {
+	
+				S = this;
+
+				for (int i=0; i<numBoids; i++) {
+						Instantiate (boidPrefab);
+				}
+		}
+	// Update is called once per frame
+	void LateUpdate () {
+	
+		Vector3 mousePos2d = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, this.transform.position.y);
+		mousePos = this.camera.ScreenToWorldPoint(mousePos2d);
+	}
+}
